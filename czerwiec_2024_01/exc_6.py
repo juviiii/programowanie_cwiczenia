4dd82e1e115acda44b606f3eec4e8ce9e42b8251
juviiii <hubertbogu@gmail.com>
2024-06-01 09:13:55 +0200

    test

---
+++ czerwiec_2024_01/exc_6.py
@@ -0,0 +1,13 @@
+numbers = []
+numbersEven = 0
+
+for i in range(10):
+    number = int(input(f"Type number {i+1}: "))
+    numbers.append(number)
+
+    if number % 2 == 0:
+        numbersEven += 1
+
+print("NUmbers you typed:", numbers)
+
+print(f"How many numbers of above are even: {numbersEven}")
